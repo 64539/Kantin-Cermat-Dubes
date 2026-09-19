cfcc8a533eddbbd4e7e1891acfb73c184aa4cb7f
Jabriel <jabrielsrizkiarjati2311@gmail.com>
2026-09-20 02:06:31 +0700

    refactor: simplify prisma service and inject environment variables via compose

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,17 +1,9 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from 'src/generated/prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
-  constructor() {
-    const adapter = new PrismaMariaDb({
-      host: process.env.DATABASE_HOST!,
-      user: process.env.DATABASE_USER!,
-      password: process.env.DATABASE_PASSWORD!,
-      database: process.env.DATABASE_NAME!,
-      port: Number(process.env.DATABASE_PORT!),
-    });
-    super({ adapter });
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
   }
 }
